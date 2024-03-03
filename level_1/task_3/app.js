7cc4e287c9bdfb15feadd0ca84cd2c9441ecde85
Sifiso <dr96mabuza@gmail.com>
2024-03-03 14:34:33 +0200

    calculator function created

---
+++ level_1/task_3/app.js
@@ -0,0 +1,30 @@
+const calculator = () => {
+    /**
+     * 
+     * @param {Number} num1 
+     * @param {Number} num2 
+     */
+    const addition = (num1, num2) => {}
+
+    /**
+     * 
+     * @param {Number} num1 
+     * @param {Number} num2 
+     */
+    const subtraction = (num1, num2) => {}
+    /**
+     * 
+     * @param {Number} num1 
+     * @param {Number} num2 
+     */
+    const division = (num1, num2) => {}
+
+    /**
+     * 
+     * @param {Number} num1 
+     * @param {Number} num2 
+     */
+    const multiplication = (num1, num2) => {}
+
+    return {addition, multiplication, addition, subtraction}
+}
