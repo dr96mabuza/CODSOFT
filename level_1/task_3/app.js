ccb1b96cdca3a4a3892936cc06fd132abb3c0567
Sifiso <dr96mabuza@gmail.com>
2024-03-03 14:52:46 +0200

    division function done for whole numbers only. and calculator return fix.

--- level_1/task_3/app.js
+++ level_1/task_3/app.js
@@ -1,8 +1,10 @@
 const calculator = () => {
     /**
      * Addition function takes only two whole numbers and return the sum.
+     * eg addition(1, 1) returns 2.
      * @param {Number} num1 
      * @param {Number} num2 
+     * @returns {Number}
      */
     const addition = (num1, num2) => {
         return Number(num1) + Number(num2);
@@ -10,18 +12,25 @@ const calculator = () => {
 
     /**
      * Subtraction function takes in two numbers and returns the difference.
+     * eg subtraction(1, 2) returns -1.
      * @param {Number} num1 
      * @param {Number} num2 
+     * @returns {Number}
      */
     const subtraction = (num1, num2) => {
         return num1 - num2;
     }
+
     /**
-     * 
+     * Divition function takes in two numbers and divides the first number by the second and return the result.
+     * eg division(2, 2) returns 1.
      * @param {Number} num1 
      * @param {Number} num2 
+     * @returns {Number}
      */
-    const division = (num1, num2) => {}
+    const division = (num1, num2) => {
+        return num1 / num2;
+    }
 
     /**
      * 
@@ -30,5 +39,5 @@ const calculator = () => {
      */
     const multiplication = (num1, num2) => {}
 
-    return {addition, multiplication, addition, subtraction}
+    return {addition, multiplication, division, subtraction}
 }
